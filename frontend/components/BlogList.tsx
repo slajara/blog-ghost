d8e05bdec036c108aa4d4b3cbbb6d2348b9fbee8
slajara <lajara.agullo@gmail.com>
2024-06-25 22:52:01 +0100

    Create BlogList Component

--- frontend/components/BlogList.tsx
+++ frontend/components/BlogList.tsx
@@ -1,4 +1,5 @@
 import { Post } from "@/typings";
+import Image from "next/image";
 
 type Props = {
   posts: Post[];
@@ -12,7 +13,19 @@ function BlogList({ posts }: Props) {
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
           <div key={post.id}>
-            <p className="font-bold">{post.title} </p>
+            <div className="relative w-full h-80 drop-shadow-xl group-hover:scale-105 transition-transform duration-200 ease-out">
+              <Image
+                className="object-cover object-left lg:object-center"
+                src="/Kitsune-whitout-blackground.png"
+                alt={post.excerpt}
+                fill
+                sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
+                priority={true}
+              />
+              <div>
+                <p className="font-bold">{post.title} </p>
+              </div>
+            </div>
           </div>
         ))}
       </div>
