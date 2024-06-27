065c2615a9f5cc1fd81c64ff86b68cd235cfaae7
slajara <lajara.agullo@gmail.com>
2024-06-27 10:54:41 +0100

    Structure typing

--- frontend/components/BlogList.tsx
+++ frontend/components/BlogList.tsx
@@ -8,7 +8,7 @@ type Props = {
 function BlogList({ posts }: Props) {
   return (
     <div>
-      <hr className="border-[#F7Ab0A] mb-10" />
+      <hr className="border-[#0AABF7] mb-10" />
 
       <div className="grid grid-cols-1 md:grid-cols-2 px-10 gap-10 gap-y-16 pb-24">
         {posts.map((post) => (
@@ -17,7 +17,7 @@ function BlogList({ posts }: Props) {
               <Image
                 className="object-cover object-left lg:object-center"
                 src="/Kitsune-whitout-blackground.png"
-                alt={post.excerpt}
+                alt={post.created_at}
                 fill
                 sizes="(max-width: 600px) 100vw, (max-width: 1200px) 50vw, 33vw"
                 priority={true}
--- frontend/typings.d.ts
+++ frontend/typings.d.ts
@@ -1,5 +1,40 @@
-export interface Post {
-    id: string;
-    title: string;
-    excerpt: string;
-  }
+// Base type for common fields
+export type Base = {
+  id: string;
+  created_at: string;
+  updated_at: string;
+  slug: string;
+};
+
+// Extends Base with specific fields for a post
+export interface Post extends Base {
+  title: string;
+  html: string; // Ghost returns HTML content by default
+  feature_image?: string; // Optional featured image URL
+  custom_excerpt?: string; // Optional custom excerpt
+  tags?: Tag[]; // Optional array of tags
+  authors?: Author[]; // Optional array of authors
+}
+
+// Author details as provided by Ghost
+export interface Author extends Base {
+  name: string;
+  profile_image?: string; // Optional profile image URL
+  cover_image?: string; // Optional cover image URL
+  bio?: string; // Optional biography
+  website?: string; // Optional website URL
+  location?: string; // Optional location
+}
+
+// Image type simplified for use with feature images and author images
+export interface Image {
+  url: string; // URL to the image
+  caption?: string; // Optional image caption
+}
+
+// Tag details as provided by Ghost
+export interface Tag extends Base {
+  name: string;
+  description?: string; // Optional description
+  feature_image?: string; // Optional featured image URL
+}
